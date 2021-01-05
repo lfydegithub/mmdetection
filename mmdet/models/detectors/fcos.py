7fe6c69469d48fc3b499bc08f0451d9358ecf5da
贺知章-刘方原 <liufangyuan@holomatic.com>
2021-01-05 16:40:43 +0800

    Test: test fcos onnx

--- mmdet/models/detectors/fcos.py
+++ mmdet/models/detectors/fcos.py
@@ -1,3 +1,5 @@
+import torch
+
 from ..builder import DETECTORS
 from .single_stage import SingleStageDetector
 
@@ -15,3 +17,32 @@ class FCOS(SingleStageDetector):
                  pretrained=None):
         super(FCOS, self).__init__(backbone, neck, bbox_head, train_cfg,
                                    test_cfg, pretrained)
+
+    def simple_test(self, img, img_metas, rescale=False):
+        """Test function without test time augmentation.
+
+        Args:
+            imgs (list[torch.Tensor]): List of multiple images
+            img_metas (list[dict]): List of image information.
+            rescale (bool, optional): Whether to rescale the results.
+                Defaults to False.
+
+        Returns:
+            list[list[np.ndarray]]: BBox results of each image and classes.
+                The outer list corresponds to each image. The inner list
+                corresponds to each class.
+        """
+        x = self.extract_feat(img)
+        outs = self.bbox_head(x)
+        # skip post-processing when exporting to ONNX
+        if torch.onnx.is_in_onnx_export():
+            return outs
+
+        bbox_list = self.bbox_head.get_bboxes(
+            *outs, img_metas, rescale=rescale)
+
+        bbox_results = [
+            bbox2result(det_bboxes, det_labels, self.bbox_head.num_classes)
+            for det_bboxes, det_labels in bbox_list
+        ]
+        return bbox_results
